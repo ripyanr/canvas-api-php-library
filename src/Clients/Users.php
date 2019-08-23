d9fd101c3f7eb739010f44989a8890adb6737369
Matt Libera <mslibera@uncg.edu>
2019-08-23 14:34:34 -0400

    first stab at most every Users endpoint

---
+++ src/Clients/Users.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace Uncgits\CanvasApi\Clients;
+
+use Uncgits\CanvasApi\CanvasApiClient;
+use Uncgits\CanvasApi\CanvasApiResult;
+
+/**
+ * https://canvas.instructure.com/doc/api/accounts.html
+ */
+class Users extends CanvasApiClient
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Methods
+    |--------------------------------------------------------------------------
+    */
+
+    public function listUsersInAccount($account_id, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('accounts/' . $account_id . '/users', 'get'));
+    }
+
+    public function listActivityStream($parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/self/activity_stream', 'get'));
+    }
+
+    public function activityStreamSummary()
+    {
+        return new CanvasApiResult($this->paginate('users/self/activity_stream/summary', 'get'));
+    }
+
+    public function listTodoItems($parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/self/todo', 'get'));
+    }
+
+    public function listCountsForTodoItems($parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/self/todo_item_count', 'get'));
+    }
+
+    public function listUpcomingAssignmentsAndCalendarEvents()
+    {
+        return new CanvasApiResult($this->paginate('users/self/upcoming_events', 'get'));
+    }
+
+    public function listMissingSubmissions($user_id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/' . $user_id . '/missing_submissions', 'get'));
+    }
+
+    public function hideStreamItem($id)
+    {
+        return new CanvasApiResult($this->call('users/self/activity_stream/' . $id, 'delete'));
+    }
+
+    public function hideAllStreamItems()
+    {
+        return new CanvasApiResult($this->call('users/self/activity_stream', 'delete'));
+    }
+
+    public function uploadFile($user_id, $local_file_path)
+    {
+        // TODO: multi-step process to get AWS address and key - skip for now.
+    }
+
+    public function showUserDetails($id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $id, 'get'));
+    }
+
+    public function createUser($account_id, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('accounts/' . $account_id . '/users', 'post'));
+    }
+
+    public function selfRegisterUser($account_id, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('accounts/' . $account_id . '/self_registration', 'post'));
+    }
+
+    public function getUserSettings($id = 'self')
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/settings', 'get'));
+    }
+
+    public function updateUserSettings($id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $id . '/settings', 'put'));
+    }
+
+    public function getCustomColors($id = 'self')
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/colors', 'get'));
+    }
+
+    public function getCustomColor($id = 'self', $asset_string)
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/colors/' . $asset_string, 'get'));
+    }
+
+    public function updateCustomColor($id = 'self', $asset_string, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $id . '/colors/' . $asset_string, 'put'));
+    }
+
+    // NOTE - listed as BETA endpoint
+    public function getDashboardPositions($id = 'self')
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/dashboard_positions', 'get'));
+    }
+
+    // NOTE - listed as BETA endpoint
+    public function updateDashboardPositions($id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $id . '/dashboard_positions', 'put'));
+    }
+
+    public function editUser($id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $id, 'put'));
+    }
+
+    public function mergeUserIntoAnotherUser($id, $destination_user_id, $destination_account_id = null)
+    {
+        if (!is_null($destination_account_id)) {
+            return new CanvasApiResult(
+                $this->call('users/' . $id . '/merge_into/accounts/' . $destination_account_id .
+                    '/users/' . $destination_user_id, 'put')
+            );
+        }
+        return new CanvasApiResult($this->call('users/' . $id . '/merge_into/' . $destination_user_id, 'put'));
+    }
+
+    public function splitMergedUsersIntoSeparateUsers($id)
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/split', 'post'));
+    }
+
+    public function getPandataEventsJwtToken($parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/self/pandata_events_token', 'post'));
+    }
+
+    public function getMostRecentlyGradedSubmissions($id = 'self')
+    {
+        return new CanvasApiResult($this->call('users/' . $id . '/graded_submissions', 'get'));
+    }
+
+    public function listAvaterOptions($user_id = 'self')
+    {
+        return new CanvasApiResult($this->call('users/' . $user_id . '/avatars', 'get'));
+    }
+
+    public function listUserPageViews($user_id = 'self', $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/' . $user_id . '/page_views', 'get'));
+    }
+
+    public function storeCustomData($user_id = 'self', $scope = null, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $user_id . '/custom_data' . $this->applyScope($scope), 'put'));
+    }
+
+    public function loadCustomData($user_id = 'self', $scope = null, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $user_id . '/custom_data' . $this->applyScope($scope), 'get'));
+    }
+
+    public function deleteCustomData($user_id = 'self', $scope = null, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->call('users/' . $user_id . '/custom_data' . $this->applyScope($scope), 'delete'));
+    }
+
+    public function listCourseNicknames()
+    {
+        return new CanvasApiResult($this->paginate('users/self/course_nicknames', 'get'));
+    }
+
+    public function getCourseNickname($course_id)
+    {
+        return new CanvasApiResult($this->paginate('users/self/course_nicknames/' . $course_id, 'get'));
+    }
+
+    public function setCourseNickname($course_id, $parameters = [])
+    {
+        $this->setParameters($parameters);
+        return new CanvasApiResult($this->paginate('users/self/course_nicknames/' . $course_id, 'put'));
+    }
+
+    public function removeCourseNickname($course_id)
+    {
+        return new CanvasApiResult($this->paginate('users/self/course_nicknames/' . $course_id, 'delete'));
+    }
+
+    public function clearCourseNicknames()
+    {
+        return new CanvasApiResult($this->paginate('users/self/course_nicknames', 'delete'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helpers
+    |--------------------------------------------------------------------------
+    */
+
+    private function applyScope(string $scope)
+    {
+        $scopeString = '';
+        if (!is_null($scope)) {
+            if (strpos($scope, '/') != 0) {
+                $scopeString .= '/';
+            }
+            $scopeString .= $scope;
+        }
+        return $scopeString;
+    }
+}
